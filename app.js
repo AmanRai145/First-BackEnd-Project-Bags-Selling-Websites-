3398ca9c80adb5ad94154549ef2b274c7d018868
AmanRai145 <amanrai0745@gmail.com>
2024-09-29 11:22:10 +0530

    one more commit

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const db = require("./config/mongoose-connection");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(_dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) {
+app.get("/", (req, res) => {
 res.send("hey");
 });
 
