ee2fff2bf870ff20359085d8840fb3b4065858bd
AmanRai145 <amanrai0745@gmail.com>
2024-10-01 00:53:13 +0530

    commit num 6

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -7,9 +7,10 @@ mongoose
 .connect(`${config.get("MONGODB_URI")}/mybagweb`)
 .then(function(){
     dbgr("connected"); //comand to set environment variable is ($env:DEBUG="development:*"
+                       // i have set the environment variable to development with the help of $env:NODE_ENV="development" this code. 
 })
-    
-
+ 
 .catch(function(err){
     console.log(err);  // This will catch and log any connection error
 });
+console.log(process.env.NODE_ENV);
--- models/owner-model.js
+++ models/owner-model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const userSchema = mongoose.Schema({
+const ownerSchema = mongoose.Schema({
     fullname: {
         type: String,
         minLength: 3,
